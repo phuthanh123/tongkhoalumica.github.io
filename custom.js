5ccf6e09f01e9b6626276071537e350fa810b5e7
phuthanh123 <35132311+phuthanh123@users.noreply.github.com>
2018-01-05 20:26:02 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -26,7 +26,7 @@ jQuery(document).ready(function($) {
     });
 	function get_data(id){
 		if(id){
-			var url = 'https://tongkhoalu.com/wp-json/wp/v2/posts?per_page=50&categories=96'+id;
+			var url = 'https://tongkhoalu.com/wp-json/wp/v2/posts?per_page=50&categories='+id;
 		} else{
 			var url = 'https://tongkhoalu.com/wp-json/wp/v2/posts?per_page=50';
 		}

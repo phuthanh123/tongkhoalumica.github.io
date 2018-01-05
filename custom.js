af64fdc7ec36437a35371110996df46fbb445500
phuthanh123 <35132311+phuthanh123@users.noreply.github.com>
2018-01-05 20:49:48 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-	$.getJSON( "https://tongkhoalu.com/wp-json/wp/v2/posts?categories=96", function(data) {
+	$.getJSON( "https://tongkhoalu.com/wp-json/wp/v2/events/?categories=23&categories_exclude=42&per_page=1", function(data) {
 		list_cat = [];
 		list_name = [];
         $.each(data, function(id,item) {
